7e52f1bd6b49821e81da4e403e104fdb834d33d7
NooaV-M <nooa.vainio-mattila@metropolia.fi>
2026-02-27 13:22:20 +0200

    week 6 assignment Jenkinsfile

---
+++ Week6-Assignment/src/main/java/TemperatureConverter.java
@@ -0,0 +1,18 @@
+public class TemperatureConverter {
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5.0 / 9.0;
+    }
+
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9.0 / 5.0) + 32;
+    }
+
+    public double kelvinToCelsius(double kelvin) {
+        return kelvin - 273.15;
+    }
+
+    public boolean isExtremeTemperatureCelsius(double celsius) {
+        return celsius < -40 || celsius > 50;
+    }
+}
+
